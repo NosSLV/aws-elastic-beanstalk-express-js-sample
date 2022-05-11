98df5bdf03bd8bceb0357094ebd811ef81aea770
NosSLV <90413107+NosSLV@users.noreply.github.com>
2022-05-11 11:32:13 +0200

    Change line 5 last time

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello NosSLV!'));
+app.get('/', (req, res) => res.send('Hello NosSLV, this is the last time we see each other!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
